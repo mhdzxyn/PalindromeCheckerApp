b8c6b09ccec02e48fcedbdc171dce9a04a657dd4
azeesmax <ah7547@srmist.edu.in>
2026-03-02 10:06:42 +0530

    uc1 commited

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp   {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("version 1.0");
+        System.out.println("System initialized");
+    }
+}
